b067aa4290f625f8ac70766d597f2d0754adad78
Adir Noyman <ofahol2007@hotmail.com>
2023-08-20 19:59:40 +0300

    Added dashboard

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,29 @@
 import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow, Logo } from '../components';
 
 const Login = () => {
   return (
-    <div className="">
-      <h1>Login</h1>
-      <Link to="/register">Register page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>login</h4>
+        <FormRow type="email" name="email" defaultValue="koko@loko.com" />
+        <FormRow type="password" name="password" defaultValue="secret123" />
+        <button type="submit" className="btn btn-block">
+          login
+        </button>
+        <button type="button" className="btn btn-block">
+          explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to="/register" className="member-btn">
+            Sign up
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
