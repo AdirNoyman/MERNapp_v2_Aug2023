615ee0c85afe5f7abe8b14d6a590c95ecd5920f6
Adir Noyman <ofahol2007@hotmail.com>
2023-08-26 19:22:03 +0300

    Added smal side bar menu

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -1,12 +1,59 @@
 import { Outlet } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/Dashboard';
+import { BigSidebar, NavBar, SmallSidebar } from '../components';
+import { useState, useContext, createContext } from 'react';
+
+const DashboardContext = createContext();
 
 const DashboardLayout = () => {
+  // temp
+  const user = { name: 'John' };
+
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleSideBar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
+  const toggleDarkTheme = () => {
+    setIsDarkTheme('Toggling dark theme');
+  };
+
+  const logoutUser = async (loggedUser) => {
+    console.log(`Logging out user ${loggedUser}...`);
+  };
+
   return (
-    <div>
-      {' '}
-      <Outlet />
-    </div>
+    <DashboardContext.Provider
+      value={{
+        user,
+        showSidebar,
+        isDarkTheme,
+        toggleDarkTheme,
+        toggleSideBar,
+        logoutUser,
+      }}
+    >
+      <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar />
+          <BigSidebar />
+          <div>
+            <NavBar />
+            <div className="dashboard-page">
+              {/* Outlet renders all of the subroutes of the dashboard route */}
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </DashboardContext.Provider>
   );
+};
+
+export const useDashboardContext = () => {
+  return useContext(DashboardContext);
 };
 
 export default DashboardLayout;
