e2a04c98f50612656795651b1155a52cfa774354
Adir Noyman <ofahol2007@hotmail.com>
2023-09-04 22:40:34 +0300

    Added more side bar functionality

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -2,33 +2,22 @@ import { FaTimes } from 'react-icons/fa';
 import Wrapper from '../assets/wrappers/SmallSidebar';
 import { useDashboardContext } from '../pages/DashboardLayout';
 import Logo from './Logo';
-import links from '../utils/links';
-import { NavLink } from 'react-router-dom';
+import NavLinks from './NavLinks';
 
 const SmallSidebar = () => {
-  const data = useDashboardContext();
+  const { showSidebar, toggleSideBar } = useDashboardContext();
 
   return (
     <Wrapper>
-      <div className="sidebar-container show-sidebar">
+      <div className={`sidebar-container ${showSidebar && 'show-sidebar'}`}>
         <div className="content">
-          <button type="button" className="close-btn">
+          <button type="button" className="close-btn" onClick={toggleSideBar}>
             <FaTimes />
           </button>
           <header>
             <Logo />
           </header>
-          <div className="nav-links">
-            {links.map((link) => {
-              const { text, path, icon } = link;
-              return (
-                <NavLink to={path} key={text} className="nav-link">
-                  <span className="icon">{icon}</span>
-                  {text}
-                </NavLink>
-              );
-            })}
-          </div>
+          <NavLinks />
         </div>
       </div>
     </Wrapper>
